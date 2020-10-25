f89aaed8905de761119f1bd9cc96842bf4dbcbea
Denis <crippton@gmail.com>
2020-10-25 10:20:32 +0300

    feat: quotes

--- momentum/script.js
+++ momentum/script.js
@@ -4,11 +4,18 @@ const time = document.getElementById('time'),
     dayOfweek = document.getElementById('dayOfWeek'),
     greeting = document.getElementById('greeting'),
     name = document.getElementById('name'),
-    focus = document.getElementById('focus');
-quote = document.getElementById('quote');
-quote_author = document.getElementById('quote_author');
+    focus = document.getElementById('focus'),
+    quote = document.getElementById('quote'),
+    quote_author = document.getElementById('quote_author'),
+    quoteUrl = 'https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=ru',
+    weatherIcon = document.querySelector('.weather_icon'),
+    temperature = document.querySelector('.weather_degree'),
+    weatherDescription = document.querySelector('.weather_city');
 
-options = {
+let backgrnds = [];
+let timeOfDay = 'default';
+
+let options = {
     weekday: 'long',
     year: 'numeric',
     month: 'long',
@@ -17,7 +24,7 @@ options = {
 
 
 // Show Time
-function showTime() {
+function showTimeGreet() {
     let today = new Date(),
         hour = today.getHours(),
         min = today.getMinutes(),
@@ -29,8 +36,18 @@ function showTime() {
     time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
     date.innerHTML = `${day}`;
     // dayOfweek.innerHTML = `${dayOfWeek}`;
+    if (6 <= hour < 12) {
+        timeOfDay = 'morning';
+    } else if (12 <= hour < 18) {
+        timeOfDay = 'day';
+    } else if (18 <= hour < 24) {
+        timeOfDay = 'evening';
+    } else {
+        timeOfDay = 'night';
+    }
 
-    setTimeout(showTime, 1000);
+    setTimeout(showTimeGreet, 1000);
+    setGreetBgrnd();
 }
 // Add Zeros
 function addZero(n) {
@@ -38,22 +55,62 @@ function addZero(n) {
 }
 
 
-function setGreet() {
+function setGreetBgrnd() {
     let today = new Date(),
-        hour = today.getHours();
-
-    if (6 <= hour < 12) {
-        greeting.textContent = ('С чудестным утром,');
-    } else if (12 <= hour < 18) {
-        greeting.textContent = ('Добырый день,');
-    } else if (18 <= hour < 24) {
-        greeting.textContent('Добрый вечер, ');
-    } else {
-        greeting.textContent('Доброй ночи, ');
+        hour = today.getHours(),
+        temp_hour = null;
+    if (temp_hour === null) {
+        temp_hour = hour;
+        if (timeOfDay = 'morning') {
+            greeting.textContent = ('С чудестным утром,');
+            document.body.style.backgroundImage = `url(${backgrnds[hour]})`;
+        } else if (timeOfDay = 'day') {
+            greeting.textContent = ('Хорошего дня,');
+            document.body.style.backgroundImage = `url(${backgrnds[hour]})`;
+        } else if (timeOfDay = 'evening') {
+            greeting.textContent('Добрый вечер, ');
+            document.body.style.backgroundImage = `url(${backgrnds[hour]})`;
+        } else {
+            greeting.textContent('Доброй ночи, ');
+            document.body.style.backgroundImage = `url(${backgrnds[hour]})`;
+        }
+    } else if (temp_hour < hour) {
+        temp_hour = null;
     }
 }
 
+function backgrndTimeOfDay(timeOfday) {
+    let backgrounds = [];
+    for (let index = 1; index <= 20; index++) {
+        index = parseInt(index, 10) < 10 ? '0' + index : +index;
+        backgrounds.push(`'../momentum/assets/images/${timeOfday}/${index}.jpg'`);
+    }
+    return backgrounds;
+}
+
+function backgrndWholeDay() {
+    let morning = backgrndTimeOfDay('morning').sort(() => .5 - Math.random()).slice(0, 6);
+    let day = backgrndTimeOfDay('day').sort(() => .5 - Math.random()).slice(0, 6);
+    let evening = backgrndTimeOfDay('evening').sort(() => .5 - Math.random()).slice(0, 6);
+    let night = backgrndTimeOfDay('night').sort(() => .5 - Math.random()).slice(0, 6);
+
+    backgrnds = backgrnds.concat(night, morning, day, evening);
+}
+
+
+async function getQuote() {
+    const res = await fetch(quoteUrl);
+    const data = await res.json();
+    quote.textContent = data.quoteText;
+    quote_author.textContent = data.quoteAuthor;
+}
+
+
+function setBackground(hour) {
+
+}
 
 //run
-
-showTime();
+backgrndWholeDay();
+showTimeGreet();
+document.addEventListener('DOMContentLoaded', getQuote);
